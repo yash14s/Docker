7729efaa8a1164485c1f10d3931aa822655d66ea
Yash <yash33srivastava@gmail.com>
2023-03-18 20:49:13 -0400

    new file:   Dockerfiles/LIDAR/Dockerfile 	modified:   docker_exec.sh 	modified:   docker_run.sh 	modified:   instructions.txt

--- docker_exec.sh
+++ docker_exec.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-CONTAINER_ID=$(sudo docker ps -a | grep fetch_sim:latest | awk '{print $1}')
+CONTAINER_ID=$(sudo docker ps -a | grep lidar:latest | awk '{print $1}')
 sudo docker exec -it $CONTAINER_ID bash
--- docker_run.sh
+++ docker_run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 sudo xhost +
-sudo docker run -d -it --rm --name ubuntu_18_container \
-	-v $HOME/catkin_ws:/root/catkin_ws \
+sudo docker run -d -it --rm --name lidar_container \
+	-v $HOME/lidar_ws:/root/lidar_ws \
 	--env DISPLAY=$DISPLAY \
 	-v /tmp/.X11-unix/:/tmp/.X11-unix/:rw \
 	--env="QT_X11_NO_MITSHM=1" \
-	-w="/root/catkin_ws" \
-	fetch_sim:latest bash
+	-w="/root/lidar_ws" \
+	lidar:latest
