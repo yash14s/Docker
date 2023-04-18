8fdf360f519a63aff3f362f26bfbe941e8d61b19
Yash <yash33srivastava@gmail.com>
2023-04-18 00:57:49 -0400

    with mavros

--- lidar_drone/docker_kill.sh
+++ lidar_drone/docker_kill.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-CONTAINER_ID=$(sudo docker ps -a | grep yash14sgatech/px4-dev-ros-melodic:v1 | awk '{print $1}')
+CONTAINER_ID=$(sudo docker ps -a | grep yash14sgatech/px4-dev-ros-melodic:v2 | awk '{print $1}')
 sudo docker kill $CONTAINER_ID
